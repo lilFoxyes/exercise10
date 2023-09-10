d4083d376eb1bf7e703fab981561cfcd9ee21c24
Foxyes <108577021+lilFoxyes@users.noreply.github.com>
2023-09-10 17:22:09 -0300

    exercise10.py

--- exercise10.py
+++ exercise10.py
@@ -1,2 +1,2 @@
 n = int(input("Me diga quantos reais você tem e te direi quantos dólares você pode comprar: "))
-print("A quantidade de dólares que você pode comprar com {} reais é: {:5}".format(n, n/4.97))
+print("A quantidade de dólares que você pode comprar com {} reais é: {:.2f}".format(n, n/4.97))
